6b9c33cbcc2bc1c7cbb873c5983379ddfbdf8d2e
nvlight <lightstyle11@mail.ru>
2023-03-06 23:09:01 +0300

    adding layout for login/request && reset password

--- vite-vue/src/views/RequestPassword.vue
+++ vite-vue/src/views/RequestPassword.vue
@@ -1,34 +1,24 @@
 <template>
-    <GuestLayout title="Войти в свой аккаунт">
+    <GuestLayout title="Запрос нового пароля">
         <form class="mt-8 space-y-6" action="#" method="POST">
             <input type="hidden" name="remember" value="true"/>
             <div class="-space-y-px rounded-md shadow-sm">
                 <div>
                     <label for="email-address" class="sr-only">Емейл</label>
                     <input id="email-address" name="email" type="email" autocomplete="email" required=""
-                           class="relative block w-full rounded-t-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                           class="relative block w-full rounded-t-md rounded-b-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                            placeholder="Емейл"/>
                 </div>
-                <div>
-                    <label for="password" class="sr-only">Пароль</label>
-                    <input id="password" name="password" type="password" autocomplete="current-password" required=""
-                           class="relative block w-full rounded-b-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                           placeholder="Пароль"/>
-                </div>
+
             </div>
 
-            <div class="flex items-center justify-between">
-                <div class="flex items-center">
-                    <input id="remember-me" name="remember-me" type="checkbox"
-                           class="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600"/>
-                    <label for="remember-me" class="ml-2 block text-sm text-gray-900">Запомнить меня</label>
-                </div>
+            <div class="flex items-center justify-end">
 
                 <div class="text-sm">
                     <router-link
-                        :to="{name: 'request-password'}"
+                        :to="{name: 'login'}"
                         class="font-medium text-indigo-600 hover:text-indigo-500">
-                        Забыли пароль?
+                        Войти в аккаунт
                     </router-link>
                 </div>
             </div>
@@ -39,7 +29,7 @@
                         <span class="absolute inset-y-0 left-0 flex items-center pl-3">
                           <LockClosedIcon class="h-5 w-5 text-indigo-500 group-hover:text-indigo-400" aria-hidden="true"/>
                         </span>
-                    <span>Войти</span>
+                    <span>Запросить</span>
                 </button>
             </div>
         </form>
